b9b467c20f6304e1f648c84c0db51d5366f5342e
Scott <scottobrien93@outlook.com>
2024-12-29 13:10:12 +0000

    Rendered the first question and its options by hardcoding

--- new-years-quiz-react/src/Components/Quiz.jsx
+++ new-years-quiz-react/src/Components/Quiz.jsx
@@ -1,9 +1,19 @@
 // Manages the quiz logic and displays the questions
 
 import React from "react";
+import questions from "../Data/questions";
 
 const Quiz = () => {
-  return <div>Your quiz is displayed here!</div>;
+  return (
+    <>
+      <div className="question-text">{questions[0].question}</div>
+      <div className="answer-section">
+        {questions[0].options.map((option, index) => {
+          return <button key={index}>{option}</button>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Quiz;
